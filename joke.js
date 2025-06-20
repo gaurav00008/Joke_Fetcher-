ca11d4a0ec56f12e4081bc20e4a462cd5e810ee0
gaurav00008 <gauravpaneru95@gmail.com>
2025-06-20 11:15:14 +0545

    Source code

--- joke.js
+++ joke.js
@@ -7,12 +7,10 @@ const axios = require('axios');
 const chalk = require('chalk');
 console.log(chalk.red('Oops!'));
 
-
-
 async function getJoke() {
   try {
     const res = await axios.get('https://v2.jokeapi.dev/joke/Programming?type=single');
-    console.log(chalk.green('\nHere’s a programming joke for you:'));
+    console.log(chalk.green('\nThe programming joke of the day is:'));
     console.log(chalk.yellow(res.data.joke));
   } catch (err) {
     console.error(chalk.red('Oops! Something went wrong:'), err.message);
